3566b0f6526ee4abada68870f443bf694c780541
pokotyamu <pokotyamu@gmail.com>
2015-11-13 19:00:44 +0900

    add data Table

--- src/data/Cell.java
+++ src/data/Cell.java
@@ -11,4 +11,46 @@ package data;
  */
 public class Cell {
     
+    private String key;
+    private String row;
+    private Object value;
+
+    public Cell createCell(String key,String row,Object value){
+        return new Cell(key, row, value);
+    }
+        
+    public Cell(String key,String row){
+        this.key = key;
+        this.row = row;
+    }
+
+    public Cell(String key, String row, Object value) {
+        this.key = key;
+        this.row = row;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setRow(String row) {
+        this.row = row;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
 }
