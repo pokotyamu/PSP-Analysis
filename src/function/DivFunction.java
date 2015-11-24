57b69da2382654e4f5d349101686d6364bf95acf
pokotyamu <pokotyamu@gmail.com>
2015-11-24 16:05:39 +0900

    add arithmetic

---
+++ src/function/DivFunction.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package function;
+
+import data.Cell;
+
+/**
+ *
+ * @author pokotyamu
+ */
+public class DivFunction extends AbstractFunction{
+
+    @Override
+    protected String getFunctionName(String data1, String data2) {
+        return "DIV_"+data1+"_"+data2;
+    }
+
+    @Override
+    public Cell function(Cell c1, Cell c2) {
+        Cell c = new Cell(0);
+        try {
+            System.out.println(c1.getIntValue());
+            System.out.println(c2.getIntValue());
+            return new Cell(c1.getIntValue()/c2.getIntValue());
+        } catch (ArithmeticException e) {
+            System.out.println("hoge");
+            return c;
+        }
+    }
+}
