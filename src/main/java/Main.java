7927f0fd2f0897cfcd2669e57dbaf8b920f4e0ca
pokotyamu <pokotyamu@gmail.com>
2015-12-16 03:05:41 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,18 @@ import static spark.Spark.get;
 
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
+import data.Table;
+import data.TableFactory;
+import function.AbstractFunction;
+import function.AddFunction;
+import function.DivFunction;
+import function.MulFunction;
+import function.SubFunction;
+import parser.Parser;
+import parser.TestParser;
+
+
+
 
 public class Main {
 
@@ -21,7 +33,20 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    String hoge = "eieieieie";
-    get("/analysis",(req,res) -> hoge);
+    Parser p = new TestParser();
+    Table tb = TableFactory.createTable(p);
+    System.out.println(tb);
+    AbstractFunction add = new AddFunction();
+    tb = add.function(tb, "PlanA", "PlanT");
+    AbstractFunction sub = new SubFunction();
+    tb = sub.function(tb, "PlanT", "PlanA");
+    AbstractFunction mul = new MulFunction();
+    tb = mul.function(tb, "PlanA", "PlanT");
+    AbstractFunction div = new DivFunction();
+    tb = div.function(tb, "PlanA", "PlanT");
+    
+    get("/analysis",(req,res) -> tb.toString());
+    
+    get("/analysis",(req,res) -> "hoge");
   }
 }
